fa875d3680ffcdcad0d0c6cd3d365a869fe01989
shohag000 <shohagbiswas000@gmail.com>
2021-11-14 18:04:41 +0600

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">
+          <router-link to="/" style="text-decoration: none">Task</router-link>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <router-link to="/profile">Profile</router-link>
+        </a>
+      </li>
+    </ul>
   </div>
   <router-view />
 </template>
 
 <style lang="scss">
+html,
+body {
+  height: 100%;
+  font-size: 16px;
+  scroll-behavior: smooth;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
-
+  padding: 8px 30px;
+  background: #d7d6d3;
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none !important;
 
     &.router-link-exact-active {
       color: #42b983;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,96 +1,61 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+  <div class="task-container">
+
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div
+          class="col-auto"
+          style="background: #ffffff"
         >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+          <div style="width: 600px; height: 500px">
+            <p class="title">My task</p>
+
+            <!-- Input -->
+            <div class="d-flex" style="margin-top: 16px">
+              <input v-model="task"
+                type="text"
+                placeholder="Enter your task description"
+                class="form-control"
+              />
+              <button class="btn btn-warning rounded-0" @click="submitTask()" style="margin-left: 8px" v-if="!editedTaskIndex">Submit</button>
+              <button class="btn btn-warning rounded-0" @click="submitTask()" style="margin-left: 8px" v-if="editedTaskIndex">Update</button>
+            </div>
+
+            <!-- Task table -->
+            <div class="table-content">
+              <table class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th scope="col" class="text-center">Task</th>
+                    <th scope="col" class="text-center">Status</th>
+                    <th scope="col" class="text-center">Update</th>
+                    <th scope="col" class="text-center">Remove</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(task,index) in tasks" :key="index">
+                    <th scope="row">{{task.name}}</th>
+                    <td class="pointer" >
+                      {{task.status}}
+                    </td>
+                    <td>
+                      <div class="text-center" @click="editTask(index)">
+                        <span class="fa fa-pen"></span>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center" @click="removeTask(index)">
+                        <span class="fa fa-trash"></span>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -100,11 +65,74 @@ export default {
   props: {
     msg: String,
   },
+  data() {
+    return {
+      task : '',
+      flag : true,
+      editedTaskIndex : null,
+      tasks : [
+        {
+          name : "The very first task",
+          status : "in-progress"
+        },
+        {
+          name : "The second task",
+          status : "in-progress"
+        }
+      ]
+    }
+  },
+
+  methods : {
+    submitTask () {
+      if (this.task.length != '') {
+        if (this.editedTaskIndex != null) {
+          this.tasks[this.editedTaskIndex].name = this.task;
+          this.editedTaskIndex = null
+        }
+        else {
+          this.tasks.push(
+            {
+              name: this.task,
+              status: 'to-do'
+            }
+          )
+        }
+        this.task = '';
+        console.log(this.tasks);
+      }
+    },
+    removeTask(i) {
+      console.log(i);
+      this.tasks.splice(i);
+    },
+    editTask(i) {
+      console.log(i);
+      this.task = this.tasks[i].name;
+      this.editedTaskIndex = i;
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.task-container {
+  min-height: 500px;
+  background: #d9d9d9;
+  padding: 16px 32px;
+
+  .title {
+    font-family: sans-serif;
+    font-size: 22px;
+    font-weight: 600;
+    line-height: 32px;
+  }
+
+  .table-content {
+    margin-top: 16px;
+  }
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -118,5 +146,13 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.pointer {
+  cursor: pointer;
+}
+
+.inProgClass {
+  background: yellow;
 }
 </style>
